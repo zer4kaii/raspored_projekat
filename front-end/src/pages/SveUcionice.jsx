1fc3ca7e5d9c0f13bf2a5785eed0c55397d8ae81
StefanGordic <153027046+StefanGordic@users.noreply.github.com>
2024-07-03 19:57:36 +0200

    patch1.1
    neuspeli pokusaj pravljenja ucionice strane :(

--- front-end/src/pages/SveUcionice.jsx
+++ front-end/src/pages/SveUcionice.jsx
@@ -1,6 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import usePrivateGet from '../hooks/usePrivateGet';
-import { Link } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import './styles/SveUcionice.css';
+import AuthContext from "../context/AuthProvider";
+//import './img/logo.png';
 
 const SveUcionice = () => {
 
@@ -11,53 +14,106 @@ const SveUcionice = () => {
         setUcionice(ucioniceData);
     },[ucioniceData])
 
+    const { setAuth,auth } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const logout = async () => {
+        // if used in more components, this should be in context
+        // axios to /logout endpoint
+        setAuth({});
+        navigate('/linkpage');
+    }
+
   return (
-    <>
-    <Link to='/'>Nazad na kontrolnu tablu</Link>
-    <br/>
-    <h2>Spisak svih ucionica</h2>
-    <table className='ucioniceTabela'>
-        <thead>
-        <tr>
-            
-            <th>
-                Broj
-            </th>
-            
-            <th>
-                Tip
-            </th>
-            
-            <th>
-                Kapacitet za kolokvijum
-            </th>
-            
-            <th>
-                Kapacitet za predavanje
-            </th>
-            </tr>
-        </thead>
-        <tbody>
-            {ucionice?.map(ucionica=>{
-                return <tr key = {ucionica.broj}>
-                    <td>
-                        {ucionica.broj}
-                    </td>
-                    <td>    
-                        {ucionica.naziv}
-                    </td>
-                    <td>
-                        {ucionica.kapK}
-                    </td>
-                    <td>
-                        {ucionica.kapS}
-                    </td>
-                </tr>
-            })}
-        </tbody>
-    </table>
-    </>
-    
+      <>
+          <div className="fullnavbar">
+              <section className="p-menu1">
+                  <nav id="navbar" className="navigation" role="navigation">
+                      <input id="toggle1" type="checkbox"/>
+                      <label className="hamburger1" htmlFor="toggle1">
+                          <div className="top"></div>
+                          <div className="meat"></div>
+                          <div className="bottom"></div>
+                      </label>
+
+                      <nav className="menu1">
+                          <Link to="/">Pocetna</Link>
+                          <Link to="/admin">Admin</Link>
+                          <Link to="/sve-ucionice">Ucionice</Link>
+                          <Link to="/moje-aktivnosti">Moje aktivnosti</Link>
+                          <Link to="/trazenje-po-terminu">Zakazivanje termina</Link>
+                          <Link to="/trazenje-po-ucionici">Pretraga po ucionicama</Link>
+
+                      </nav>
+                  </nav>
+              </section>
+
+              <div className="logo">
+                  <img src={process.env.PUBLIC_URL + "/img/logo.png"} alt="Logo"/>
+              </div>
+
+              <div className="username123">
+                  <h2>Dobrodosli, {auth.korisnicko_ime} </h2>
+              </div>
+
+              <div className="logo">
+                  <img src="NEMOZEJEBENODARADI" alt="random ass slika"/>
+              </div>
+
+              <div className="logoutbutton1">
+                  <button onClick={logout}>Sign Out</button>
+              </div>
+
+          </div>
+          <br/>
+          <div className="tabela1">
+              <h2>Spisak svih ucionica</h2>
+              <table className='ucioniceTabela'>
+              <thead>
+                  <tr>
+
+                      <th>
+                          Broj
+                      </th>
+
+                      <th>
+                          Tip
+                      </th>
+
+                      <th>
+                          Kapacitet za kolokvijum
+                      </th>
+
+                      <th>
+                          Kapacitet za predavanje
+                      </th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  {ucionice?.map(ucionica => {
+                      return <tr key={ucionica.broj}>
+                          <td>
+                              {ucionica.broj}
+                          </td>
+                          <td>
+                              {ucionica.naziv}
+                          </td>
+                          <td>
+                              {ucionica.kapK}
+                          </td>
+                          <td>
+                              {ucionica.kapS}
+                          </td>
+                      </tr>
+                  })}
+                  </tbody>
+              </table>
+          </div>
+          <div className="controlpanel">
+            <Link to='/'>Nazad na kontrolnu tablu</Link>
+          </div>
+      </>
+
   )
 }
 
